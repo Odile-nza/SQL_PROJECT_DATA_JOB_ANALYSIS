964c75522302b823a367d53f442bbe79b45a2e11
Odile <41570723+Odile-nza@users.noreply.github.com>
2025-10-28 18:06:18 +0200

    Upload sql files

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -26,17 +26,17 @@ NOTE: If you are having issues with permissions. And you get error:
 -- NOTE: This has been updated from the video to fix issues with encoding
 
 COPY company_dim
-FROM 'E:\SQL\SQL_Project_Data_Job_Analysis\csv_files\company_dim.csv'
+FROM 'D:\SQL_PROJECT_DATA_JOB_ANALYSIS\csv_files\company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM 'E:\SQL\SQL_Project_Data_Job_Analysis\csv_files\skills_dim.csv'
+FROM 'D:\SQL_PROJECT_DATA_JOB_ANALYSIS\csv_files\skills_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM 'E:\SQL\SQL_Project_Data_Job_Analysis\csv_files\job_postings_fact.csv'
+FROM 'D:\SQL_PROJECT_DATA_JOB_ANALYSIS\csv_files\job_postings_fact.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM 'E:\SQL\SQL_Project_Data_Job_Analysis\csv_files\skills_job_dim.csv'
+FROM 'D:\SQL_PROJECT_DATA_JOB_ANALYSIS\csv_files\skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
